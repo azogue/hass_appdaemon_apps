2fa8db56e17c62a701405d3ab040841e9abe4a62
azogue <eugenio.panadero@gmail.com>
2017-01-19 12:02:49 +0100

    don't publish (POST in master) groups: not working; port change for SSL conexion

--- conf/apps/publish_states_in_master.py
+++ conf/apps/publish_states_in_master.py
@@ -27,6 +27,7 @@ class SlavePublisher(appapi.AppDaemon):
     _hass_url = None
     _hass_key = None
 
+    _hass_master_ssl = None
     _hass_master_url = None
     _hass_master_key = None
     _sufix = None
@@ -38,15 +39,23 @@ class SlavePublisher(appapi.AppDaemon):
 
     def initialize(self):
         """AppDaemon required method for app init."""
+
+        self._hass_master_ssl = self.args.get('master_ha_https', 'false') == 'true'
         self._hass_master_url = self.args.get('master_ha_url')
         self._hass_master_key = self.args.get('master_ha_key', '')
         self._sufix = self.args.get('slave_sufix', DEFAULT_SUFFIX)
 
-        self._excluded = self.args.get('excluded', '').split(',')
+        self._excluded = self.args.get('excluded', '')
+        if self._excluded:
+            self._excluded = self._excluded.split(',')
+        self.log('EXCLUDED: {}'.format(self._excluded))
+        self.log('EXCLUDED: {}'.format(self._excluded))
         raw_entities = self.args.get('force_update_entities', '')
         self._force_update_entities = [e.rstrip().lstrip() for e in raw_entities.split(',')]
 
-        self._master_ha_api = remote.API(self._hass_master_url, self._hass_master_key)
+        self._master_ha_api = remote.API(self._hass_master_url, self._hass_master_key,
+                                         port=80 if self._hass_master_ssl else 8123,
+                                         use_ssl=self._hass_master_ssl)
 
         # Publish slave states in master
         bs_states = self.get_state('binary_sensor')
@@ -54,33 +63,17 @@ class SlavePublisher(appapi.AppDaemon):
         sensors = dict(**s_states)
         sensors.update(bs_states)
         for entity_id, state_atts in sensors.items():
-            if not any([excl in entity_id for excl in self._excluded]):
+            if state_atts and ('homebridge_hidden' in state_atts['attributes']):
+                self.log('SENSOR: {}, hidden={}'.format(entity_id, state_atts['attributes']['homebridge_hidden']))
+            else:
+                self.log('SENSOR: {}, ATTRS={}'.format(entity_id, state_atts))
+            if not self._excluded or not any([excl in entity_id for excl in self._excluded]):
                 f_upd = entity_id in self._force_update_entities
                 self.log('--> sensor "{}": {}, FU={}'.format(entity_id, state_atts['state'], f_upd), level=LOG_LEVEL)
                 remote.set_state(self._master_ha_api, entity_id + self._sufix, state_atts['state'],
                                  attributes=state_atts['attributes'], force_update=f_upd)
                 self.listen_state(self._ch_state, entity_id, attributes=state_atts['attributes'], force_update=f_upd)
 
-        # Publish groups and logout group intersections --> append slave entities to master.group
-        master_group_names = [g.entity_id for g in remote.get_states(self._master_ha_api)
-                              if g.entity_id.startswith('group.')]
-        s_states = self.get_state('group')
-        for entity_id, state_atts in s_states.items():
-            if not any([excl in entity_id for excl in self._excluded]):
-                attrs = state_atts['attributes'].copy()
-                attrs['entity_id'] = [e + self._sufix for e in attrs['entity_id']]
-                if entity_id in master_group_names:  # Solape --> fusión
-                    master_g = remote.get_state(self._master_ha_api, entity_id)
-                    self.log('*** GROUP INTERSECT "{}". Master: {} + {}'
-                             .format(entity_id, master_g.attributes['entity_id'], attrs['entity_id']), level=LOG_LEVEL)
-                    # self.log('*** GROUP FUSION "{}". Master: {}; Slave: {}'
-                    #         .format(entity_id, master_g.attributes['entity_id'], attrs['entity_id']), level=LOG_LEVEL)
-                    # attrs['entity_id'] = master_g.attributes['entity_id'] + attrs['entity_id']
-                    # remote.set_state(self._master_ha_api, entity_id, state_atts['state'], attributes=attrs)
-                else:
-                    self.log('* GROUP "{}" --> {}'.format(entity_id + self._sufix, attrs['entity_id']), level=LOG_LEVEL)
-                    remote.set_state(self._master_ha_api, entity_id + self._sufix,
-                                     state_atts['state'], attributes=attrs)
         self.log('Transfer states from slave to master in {} COMPLETE'.format(self._master_ha_api), level='INFO')
 
     # noinspection PyUnusedLocal

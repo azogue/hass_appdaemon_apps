05a79efcac8b83c264f7242663deb017aac16599
azogue <eugenio.panadero@gmail.com>
2017-01-20 17:19:26 +0100

    simplify app for publish slave state changes to master

--- conf/apps/publish_states_in_master.py
+++ conf/apps/publish_states_in_master.py
@@ -22,17 +22,14 @@ DEFAULT_SUFFIX = '_slave'
 # noinspection PyClassHasNoInit
 class SlavePublisher(appapi.AppDaemon):
     """AppDaemon Class for setting states from one HASS instance (main) in Another (remote).
-    Valid for binary_sensors, sensors & groups."""
+    Valid for binary_sensors and sensors."""
 
     _hass_url = None
     _hass_key = None
 
-    _hass_master_ssl = None
     _hass_master_url = None
     _hass_master_key = None
     _sufix = None
-    _excluded = None
-    _force_update_entities = None
 
     _ha_api = None
     _master_ha_api = None
@@ -40,22 +37,10 @@ class SlavePublisher(appapi.AppDaemon):
     def initialize(self):
         """AppDaemon required method for app init."""
 
-        self._hass_master_ssl = self.args.get('master_ha_https', 'false') == 'true'
         self._hass_master_url = self.args.get('master_ha_url')
         self._hass_master_key = self.args.get('master_ha_key', '')
         self._sufix = self.args.get('slave_sufix', DEFAULT_SUFFIX)
-
-        self._excluded = self.args.get('excluded', '')
-        if self._excluded:
-            self._excluded = self._excluded.split(',')
-        self.log('EXCLUDED: {}'.format(self._excluded))
-        self.log('EXCLUDED: {}'.format(self._excluded))
-        raw_entities = self.args.get('force_update_entities', '')
-        self._force_update_entities = [e.rstrip().lstrip() for e in raw_entities.split(',')]
-
-        self._master_ha_api = remote.API(self._hass_master_url, self._hass_master_key,
-                                         port=80 if self._hass_master_ssl else 8123,
-                                         use_ssl=self._hass_master_ssl)
+        self._master_ha_api = remote.API(self._hass_master_url, self._hass_master_key, port=8123)
 
         # Publish slave states in master
         bs_states = self.get_state('binary_sensor')
@@ -63,24 +48,14 @@ class SlavePublisher(appapi.AppDaemon):
         sensors = dict(**s_states)
         sensors.update(bs_states)
         for entity_id, state_atts in sensors.items():
-            if state_atts and ('homebridge_hidden' in state_atts['attributes']):
-                self.log('SENSOR: {}, hidden={}'.format(entity_id, state_atts['attributes']['homebridge_hidden']))
-            else:
-                self.log('SENSOR: {}, ATTRS={}'.format(entity_id, state_atts))
-            if not self._excluded or not any([excl in entity_id for excl in self._excluded]):
-                f_upd = entity_id in self._force_update_entities
-                self.log('--> sensor "{}": {}, FU={}'.format(entity_id, state_atts['state'], f_upd), level=LOG_LEVEL)
-                remote.set_state(self._master_ha_api, entity_id + self._sufix, state_atts['state'],
-                                 attributes=state_atts['attributes'], force_update=f_upd)
-                self.listen_state(self._ch_state, entity_id, attributes=state_atts['attributes'], force_update=f_upd)
-
+            self.log('SENSOR: {}, ATTRS={}'.format(entity_id, state_atts))
+            self.log('--> sensor "{}": {}'.format(entity_id, state_atts['state']), level=LOG_LEVEL)
+            remote.set_state(self._master_ha_api, entity_id + self._sufix,
+                             state_atts['state'], attributes=state_atts['attributes'])
+            self.listen_state(self._ch_state, entity_id, attributes=state_atts['attributes'])
         self.log('Transfer states from slave to master in {} COMPLETE'.format(self._master_ha_api), level='INFO')
 
     # noinspection PyUnusedLocal
     def _ch_state(self, entity, attribute, old, new, kwargs):
-        if 'force_update' in kwargs and kwargs['force_update']:
-            # self.log('STATE CHANGE: {} from "{}" to "{}"; attr={}'.format(entity, old, new, attribute))
-            remote.set_state(self._master_ha_api, entity + self._sufix, new,
-                             attributes=self.get_state(entity, 'attributes'))
-        else:
-            remote.set_state(self._master_ha_api, entity + self._sufix, new, **kwargs)
+        remote.set_state(self._master_ha_api, entity + self._sufix, new, **kwargs)
+        # self.log('STATE CHANGE: {} from "{}" to "{}"; attr={}; kw={}'.format(entity, old, new, attribute, kwargs))
